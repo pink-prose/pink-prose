42c0f279a65bc0de641343d215170f7667cd1e5b
Meadowsys <blazeykirin@gmail.com>
2024-03-24 23:02:52 -0700

    reviewed everything, made some tweaks here and there

--- packages/auth/src/signin/server.rs
+++ packages/auth/src/signin/server.rs
@@ -4,7 +4,7 @@ pub trait ServerSigninS1: Sized {
 	type Error: From<Error>;
 
 	fn receive_request(&mut self) -> fut!(SigninS1Request);
-	fn get_salt_if_verified(&mut self) -> fut!(SigninS1GetSalt);
+	fn get_salt_if_verified(&mut self, email: &Email) -> fut!(SigninS1GetSalt);
 	fn finalise_unverified(self) -> fut!(());
 	fn finalise_invalid_email(self) -> fut!(());
 	fn store_in_progress_signin(&mut self, signin: &SigninS1InProgress) -> fut!(());
@@ -17,7 +17,7 @@ pub trait ServerSigninS1: Sized {
 				email
 			} = server.receive_request().await?;
 
-			let salt = match server.get_salt_if_verified().await? {
+			let salt = match server.get_salt_if_verified(&email).await? {
 				SigninS1GetSalt::Verified(salt) => { salt }
 				SigninS1GetSalt::Unverified => { return server.finalise_unverified().await }
 				SigninS1GetSalt::InvalidEmail => { return server.finalise_invalid_email().await }
@@ -46,7 +46,8 @@ pub trait ServerSigninS2: Sized {
 	type Error: From<Error>;
 
 	fn receive_request(&mut self) -> fut!(SigninS2Request);
-	fn fetch_in_progress_signin(&mut self) -> fut!(SigninS1InProgress);
+	fn fetch_in_progress_signin(&mut self, signin_attempt_id: &SigninAttemptID) -> fut!(SigninS1InProgress);
+	// TODO: this could be combined with `fetch_in_progress_signin`?
 	fn fetch_user_info(&mut self, signin_attempt_id: &SigninAttemptID) -> fut!(SigninS2UserInfo);
 	fn finalise_invalid_password_verifier(self) -> fut!(());
 	fn store_in_progress_signin(&mut self, signin: &SigninS2InProgress) -> fut!(());
@@ -64,7 +65,7 @@ pub trait ServerSigninS2: Sized {
 				email,
 				signin_attempt_id,
 				time
-			} = server.fetch_in_progress_signin().await?;
+			} = server.fetch_in_progress_signin(&signin_attempt_id).await?;
 
 			let SigninS2UserInfo {
 				hashed_password_verifier,
@@ -72,7 +73,10 @@ pub trait ServerSigninS2: Sized {
 				encrypted_secret_key
 			} = server.fetch_user_info(&signin_attempt_id).await?;
 
-			let hashed_password_verifier_new = HashedPasswordVerifier::from_password_verifier_and_salt(&password_verifier, &password_verifier_salt)?;
+			let hashed_password_verifier_new = HashedPasswordVerifier::from_password_verifier_and_salt(
+				&password_verifier,
+				&password_verifier_salt
+			)?;
 			let password_verifier_eq = ::constant_time_eq::constant_time_eq_32(
 				hashed_password_verifier.as_bytes(),
 				hashed_password_verifier_new.as_bytes()
@@ -104,7 +108,7 @@ pub trait ServerSigninS3: Sized {
 	type Error: From<Error>;
 
 	fn receive_request(&mut self) -> fut!(SigninS3Request);
-	fn fetch_in_progress_signin(&mut self) -> fut!(SigninS2InProgress);
+	fn fetch_in_progress_signin(&mut self, signin_attempt_id: &SigninAttemptID) -> fut!(SigninS2InProgress);
 	fn fetch_user_info(&mut self, signin_attempt_id: &SigninAttemptID) -> fut!(SigninS3UserInfo);
 	fn finalise_invalid_signature(self) -> fut!(());
 	fn store_session_info(&mut self, session_info: &SessionServerInfo) -> fut!(());
@@ -124,7 +128,7 @@ pub trait ServerSigninS3: Sized {
 				signin_attempt_id,
 				signing_challenge,
 				time
-			} = server.fetch_in_progress_signin().await?;
+			} = server.fetch_in_progress_signin(&signin_attempt_id).await?;
 
 			let SigninS3UserInfo {
 				public_key
