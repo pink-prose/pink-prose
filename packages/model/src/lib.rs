e50c6726ce562baa9cfbe23a4fba54f766cfa188
Meadowsys <blazeykirin@gmail.com>
2024-03-24 20:31:52 -0700

    create a uh, `model` package scaffold

---
+++ packages/model/src/lib.rs
@@ -0,0 +1,4 @@
+// TODO: remove later when more complete
+#![allow(dead_code, unused_imports, unused_variables)]
+
+pub mod user;
---
+++ packages/model/src/user.rs
@@ -0,0 +1,1 @@
+use ::bitcode::{ Buffer, Decode, Encode, encode, decode };
